2d146dded0f1747d5556690af3640105df6bcc18
student <student@NANO-05>
2017-05-24 11:04:11 +0200

    zmiana w serwere klient

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Adres = new System.Windows.Forms.TextBox();
+            this.przycisk_start = new System.Windows.Forms.Button();
+            this.przycisk_stop = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.port_p = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.port_p)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Adres
+            // 
+            this.Adres.Location = new System.Drawing.Point(12, 12);
+            this.Adres.Name = "Adres";
+            this.Adres.Size = new System.Drawing.Size(147, 20);
+            this.Adres.TabIndex = 0;
+            this.Adres.Text = "Adres";
+            // 
+            // przycisk_start
+            // 
+            this.przycisk_start.Location = new System.Drawing.Point(33, 296);
+            this.przycisk_start.Name = "przycisk_start";
+            this.przycisk_start.Size = new System.Drawing.Size(75, 23);
+            this.przycisk_start.TabIndex = 1;
+            this.przycisk_start.Text = "Start";
+            this.przycisk_start.UseVisualStyleBackColor = true;
+            this.przycisk_start.Click += new System.EventHandler(this.przycisk_start_Click);
+            // 
+            // przycisk_stop
+            // 
+            this.przycisk_stop.Location = new System.Drawing.Point(269, 296);
+            this.przycisk_stop.Name = "przycisk_stop";
+            this.przycisk_stop.Size = new System.Drawing.Size(75, 23);
+            this.przycisk_stop.TabIndex = 2;
+            this.przycisk_stop.Text = "Stop";
+            this.przycisk_stop.UseVisualStyleBackColor = true;
+            this.przycisk_stop.Click += new System.EventHandler(this.przycisk_stop_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(348, 212);
+            this.listBox1.TabIndex = 3;
+            // 
+            // port_p
+            // 
+            this.port_p.Location = new System.Drawing.Point(226, 12);
+            this.port_p.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.port_p.Name = "port_p";
+            this.port_p.Size = new System.Drawing.Size(120, 20);
+            this.port_p.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 343);
+            this.Controls.Add(this.port_p);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.przycisk_stop);
+            this.Controls.Add(this.przycisk_start);
+            this.Controls.Add(this.Adres);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.port_p)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Adres;
+        private System.Windows.Forms.Button przycisk_start;
+        private System.Windows.Forms.Button przycisk_stop;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NumericUpDown port_p;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Net.Sockets;
+using System.Net;
 namespace SerwTCP
 {
     public partial class Form1 : Form
@@ -15,6 +16,35 @@ namespace SerwTCP
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void przycisk_start_Click(object sender, EventArgs e)
+        {
+            IPAddress adresIP = null;
+            try
+            {
+                adresIP = IPAddress.Parse(Adres.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Błędny format adresu IP");
+                Adres.Text = String.Empty;
+                return;
+            }
+            int port = System.Convert.ToInt32(port_p.Value);
+            try
+            {
+                serwer = new TcpListener(adresIP, port);
+                serwer.Start();
+                klient = serwer.AcceptTcpClient();
+                listBox1.Items.Add("Nawiązano pol");
+            }
+        }
+        private TcpListener serwer = null;
+        private TcpClient klient = null;
+        private void przycisk_stop_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
